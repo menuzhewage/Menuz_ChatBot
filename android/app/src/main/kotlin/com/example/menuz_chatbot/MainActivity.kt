c44a78bf5d1772d6a05c9b7af6aef5e749b1e790
Menuz <menuzhewage@gmail.com>
2024-12-25 21:01:30 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/menuz_chatbot/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.menuz_chatbot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:dialog_flowtter/dialog_flowtter.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  late DialogFlowtter dialogFlowtter;
+  final TextEditingController controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
